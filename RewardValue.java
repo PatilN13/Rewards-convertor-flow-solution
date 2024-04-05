1afef323a3cb3f5b01d5706572b490fb091d74d6
PatilN13 <166065991+PatilN13@users.noreply.github.com>
2024-04-05 14:40:38 -0400

    RewardValue.java

--- RewardValue.java
+++ RewardValue.java
@@ -6,16 +6,8 @@ public class RewardValue {
         this.cashValue = cashValue;
     }
 
-    public RewardValue(int milesValue) {
-        this.cashValue = convertToCash(milesValue);
-    }
-
     private static int convertToMiles(double cashValue) {
         return (int) (cashValue / MILES_TO_CASH_CONVERSION_RATE);
-    }
-
-    private static double convertToCash(int milesValue) {
-        return milesValue * MILES_TO_CASH_CONVERSION_RATE;
     }
 
     public double getCashValue() {
